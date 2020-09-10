7fe3a177cf4216fb739b7d45136363f746cfea8d
cl-djkim <djkim@commercelab.co.kr>
2020-09-10 14:33:38 +0900

    [2020-09-10] webpack production 적용

--- resource/main.js
+++ resource/main.js
@@ -1,19 +1,43 @@
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+function reducer(state, action) {
+	if (state === undefined) {
+		return {
+			contents: [
+				{id: 1, title: 'HTML', desc: 'HTML is ...'},
+				{id: 2, title: 'CSS', desc: 'CSS is ...'},
+			],
+		};
+	}
+}
+
+window.store = createStore(reducer, composeWithDevTools(applyMiddleware()));
+
 function subject() {
 	document.querySelector('#subject').innerHTML = `
 		<header>
 			<h1>WEB</h1>
 			Hello, WEB!
-			</header>
+		</header>
 	`;
 }
 subject();
 
 function TOC() {
+	let state = store.getState(),
+		i = 0,
+		cnt = state.contents.length,
+		template = '';
+
+	for (i = 0; i < cnt; i++) {
+		template += `<li><a href="${state.contents[i].id}">${state.contents[i].title}</a></li>`;
+	}
+
 	document.querySelector('#toc').innerHTML = `
 		<nav>
 			<ol>
-				<li><a href="1.html">HTML</a></li>
-				<li><a href="2.html">CSS</a></li>
+			${template}
 			</ol>
 		</nav>
 	`;
