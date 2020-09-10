ceb6d8f16a3c9ed6ab17be6c085a0c2ad0b3aa7f
cl-djkim <djkim@commercelab.co.kr>
2020-09-10 10:33:38 +0900

    [2020-09-10] redux dispatch subscribe test

--- resource/with_redux.js
+++ resource/with_redux.js
@@ -11,7 +11,7 @@ function reducer(state, action) {
 	var newState;
 
 	if (action.type === 'CHANGE_COLOR') {
-		newState = Object.assign({}, state, {color: 'red'});
+		newState = Object.assign({}, state, {color: action.color});
 	}
 
 	return newState;
@@ -29,4 +29,30 @@ function red() {
 			`;
 }
 
+function blue() {
+	let state = store.getState();
+	document.querySelector('#blue').innerHTML = `
+				<div class="container" id="component_blue" style="background-color: ${state.color}">
+					<h1>blue</h1>
+					<input type="button" value="fire" onclick="store.dispatch({type: 'CHANGE_COLOR', color: 'blue'})">
+				</div>
+			`;
+}
+
+function green() {
+	let state = store.getState();
+	document.querySelector('#green').innerHTML = `
+				<div class="container" id="component_blue" style="background-color: ${state.color}">
+					<h1>green</h1>
+					<input type="button" value="fire" onclick="store.dispatch({type: 'CHANGE_COLOR', color: 'green'})">
+				</div>
+			`;
+}
+
+store.subscribe(red);
+store.subscribe(blue);
+store.subscribe(green);
+
 red();
+blue();
+green();
