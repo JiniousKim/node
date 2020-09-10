37fa59e473b0190ca9d35d43c0566449ad60748d
cl-djkim <djkim@commercelab.co.kr>
2020-09-10 16:38:10 +0900

    [2020-09-10] redux를 이용한 create 기능 구현

--- resource/main.js
+++ resource/main.js
@@ -6,6 +6,7 @@ function reducer(state, action) {
 	if (state === undefined) {
 		return {
 			selected_id: 1,
+			mode: 'read',
 			contents: [
 				{id: 1, title: 'HTML', desc: 'HTML is ...'},
 				{id: 2, title: 'CSS', desc: 'CSS is ...'},
@@ -13,8 +14,27 @@ function reducer(state, action) {
 		};
 	}
 
+	if (action.type === 'CREATE') {
+		return Object.assign({}, state, {mode: action.mode});
+	}
+
 	if (action.type === 'SELECT') {
-		return Object.assign({}, state, {selected_id: action.id});
+		return Object.assign({}, state, {
+			selected_id: action.id,
+			mode: action.mode,
+		});
+	}
+
+	if (action.type === 'STORE') {
+		return Object.assign({}, state, {
+			contents: state.contents.concat({
+				id: state.contents.length + 1,
+				title: action.title,
+				desc: action.desc,
+			}),
+			mode: 'read',
+			selected_id: state.contents.length + 1
+		});
 	}
 }
 
@@ -41,7 +61,7 @@ function TOC() {
 			<li>
 				<a onclick="
 					event.preventDefault();
-					store.dispatch({type: 'SELECT', id: ${state.contents[i].id}});
+					store.dispatch({type: 'SELECT', id: ${state.contents[i].id}, mode: 'read'});
 					" 
 					href="${state.contents[i].id}">
 				${state.contents[i].title}
@@ -63,7 +83,11 @@ TOC();
 function control() {
 	document.querySelector('#control').innerHTML = `
 			<ul>
-				<li><a href="/create">create</a></li>
+				<li><a onclick="
+				event.preventDefault();
+				store.dispatch({type: 'CREATE', mode: 'create'})
+				" 
+				href="/create">create</a></li>
 				<li><input type="button" value="delete"></li>
 			</ul>
 		`;
@@ -72,12 +96,36 @@ control();
 
 function article() {
 	var state = store.getState();
-	document.querySelector('#article').innerHTML = `
+
+	if (state.mode === 'read') {
+		document.querySelector('#article').innerHTML = `
+				<article>
+					<h2>${state.contents[state.selected_id - 1].title}</h2>
+					${state.contents[state.selected_id - 1].desc}
+				</article>
+			`;
+	}
+
+	if (state.mode === 'create') {
+		document.querySelector('#article').innerHTML = `
 			<article>
-				<h2>${state.contents[state.selected_id - 1].title}</h2>
-				${state.contents[state.selected_id - 1].desc}
+				<form onsubmit="
+				event.preventDefault();
+				store.dispatch({
+				  type: 'STORE',
+				  title: this.title.value,
+				  desc: this.description.value,
+				});
+				">
+					<p><input type="text" name="title" placeholder="Title"></p>
+					<p><textarea name="description" placeholder="description"></textarea></p>
+					<input type="submit" value="submit">
+				</form>
 			</article>
 		`;
+	}
 }
 article();
+
 store.subscribe(article);
+store.subscribe(TOC);
